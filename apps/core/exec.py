89af1c6148316ccba0576adedcd7c0d9a33b21a9
Freanu Peria <magnussonfrancis@gmail.com>
2019-05-08 21:19:43 +0800

    managed to make the spider work. Yay! :)

--- apps/core/exec.py
+++ apps/core/exec.py
@@ -1,15 +1,15 @@
 import scrapy
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerProcess, Crawler
 
 from scraper import GroupPostsSpider
 
 
 def scrape(inp_email, inp_pass):
-    spider = GroupPostsSpider(email=inp_email, password=inp_pass)
-    process = CrawlerProcess({
+    crawler = Crawler(GroupPostsSpider, {
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
         'ROBOTSTXT_OBEY': False,
     })
+    process = CrawlerProcess()
 
-    process.crawl(spider)
-    process.start()  # the script will block here until the crawling is finished
+    process.crawl(crawler, email=inp_email, password=inp_pass)
+    process.start()
--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    post_url = models.URLField(unique=True)
+    group = models.CharField(max_length=100)
+    group_url = models.URLField()
+    author = models.CharField(max_length=75)
+    author_url = models.URLField()
+    description = models.TextField()
+    total_reacts = models.CharField(max_length=10)
+    likes = models.CharField(max_length=10)
+    heart = models.CharField(max_length=10)
+    wow = models.CharField(max_length=10)
+    haha = models.CharField(max_length=10)
+    sad = models.CharField(max_length=10)
+    angry = models.CharField(max_length=10)
+    comments = models.ForeignKey(
+        to='Comment',
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return '{} - {}'.format(self.title, self.author)
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=75)
+    text = models.TextField()
+    time = models.CharField(max_length=50)
+    no_reacts = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.text
