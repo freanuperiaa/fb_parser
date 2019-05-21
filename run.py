19b4db70e076d103e766bf001a126e11ea370f75
Freanu Peria <magnussonfrancis@gmail.com>
2019-05-21 16:26:46 +0800

    removed run.

--- run.py
+++
@@ -1,6 +0,0 @@
-from apps.core.models import *
-from apps.core.tasks import scrape_group
-
-# FacebookGroup.objects.get('
-scrape_group('freanuperia@gmail.com', 'magnussonfrancis', 'https://mbasic.facebook.com/groups/1536495259983145')
-
